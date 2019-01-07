ba21d75ddd7bc850d7a9370bcbbff2bde385976e
Shih_Kuan <cliff1304@gmail.com>
2019-01-07 10:02:35 +0800

    建立 "更新候選人" 方法

--- app/controllers/candidates_controller.rb
+++ app/controllers/candidates_controller.rb
@@ -23,6 +23,16 @@ class CandidatesController < ApplicationController
     @candidate = Candidate.find_by(id: params[:id])
   end
 
+  def update
+    @candidate = Candidate.find_by(id: params[:id])
+
+    if @candidate.update(candidate_params)
+      redirect_to candidates_path, notice: "資料更新成功"
+    else
+      render :edit
+    end
+  end
+
   private
   def candidate_params
     params.require(:candidate).permit(:name, :age, :party, :politics)
